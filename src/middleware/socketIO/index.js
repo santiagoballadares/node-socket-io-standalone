f2dda37a3fbf450f1a8f2bb45fa55286c99c0b20
Santiago Balladares <santiago.balladares@outlook.com>
2020-09-05 16:13:06 -0500

    Add socketIO setup

--- src/middleware/socketIO/index.js
+++ src/middleware/socketIO/index.js
@@ -1,7 +1,27 @@
 const { createMessage } = require('./utils');
 
 const setupSocketIO = io => {
-  // TODO
+  io.on('connection', socket => {
+    console.log('New client connected');
+    // Send welcome message only to new client
+    socket.emit('receiveMessage', createMessage('Server', 'Welcome!'));
+  
+    // Send message to all clients except sender (connected socket)
+    socket.broadcast.emit('receiveMessage', createMessage('Server', 'New user has joined'));
+  
+    // Listen to 'sendMessage' for messages from client
+    socket.on('sendMessage', message => {
+      console.log('Message received:', message);
+      // Send the message to all clients including the sender (connected socket)
+      io.emit('receiveMessage', createMessage(message.from, message.text));
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Client disconnected');
+      // Send message to all clients except sender (connected socket)
+      socket.broadcast.emit('receiveMessage', createMessage('Server', 'User has disconnected'));
+    });
+  });
 }
 
 module.exports = {
